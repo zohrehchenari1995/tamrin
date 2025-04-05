7018b6161584847046aa81c0e25ed35669dfe913
zohrehchenari <zohrehchenari2874@gmail.com>
2025-04-05 20:26:37 +0330

    grid template

---
+++ testi.js
@@ -0,0 +1,22 @@
+// const firstOrderFunction= ()=>{
+//    console.log("im first order function")
+// }
+
+// const heigherOrderfunction= (returnFirstOrederFunction)=>{
+//   returnFirstOrederFunction();
+// }
+// heigherOrderfunction(firstOrderFunction);
+
+
+
+// // const unaryFunc = (a)=>{
+// //   return sum(a+10);
+// // }
+// // unaryFunc(20);
+
+
+
+// function increment(x) {
+// 	return sum(x,1);
+// }
+// increment(5)
